fca5ac9fcb677d25c983f077681528a53e64c4bf
nishant1546-art <nishant1546@gmail.com>
2026-06-30 09:54:14 +0530

    Restore cursor and greeting typing effects

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener('mousemove', (e) => {
 });
 
 // Typing effect for greeting
-const greetingText = "Hey You Know What! You're the most adorable human i ever met! 💖";
+const greetingText = "Hey You Know What! Just do as i ask you to do 💖";
 const greetingElement = document.querySelector('.greeting');
 let charIndex = 0;
 
